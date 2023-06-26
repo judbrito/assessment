dfd23fa94a11991209feb43c35d7318501ebbb9e
judbrito <admoceano@gmail.com>
2023-06-26 18:04:51 -0300

    Adicionado as classes Driver e excel

--- BlazeAutomation/src/test/java/core/Driver.java
+++ BlazeAutomation/src/test/java/core/Driver.java
@@ -1,5 +1,34 @@
 package core;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Driver {
+
+	private static WebDriver driver;
+
+	public static void createAcess() {
+
+		System.setProperty("webdriver.chrome.driver", "./BlazeAutomation/driverChrome/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+
+	public static WebDriver getWebDriver() {
+
+		if (driver == null) {
+			createAcess();
+		}
+		return driver;
+	}
+
+	public static void tearDown() {
+
+		if (driver != null) {
+			driver.quit();
+			driver = null;
+		}
+
+	}
 
 }
--- BlazeAutomation/src/test/java/core/MassaDados.java
+++ BlazeAutomation/src/test/java/core/MassaDados.java
@@ -1,5 +1,84 @@
 package core;
 
-public class MassaDados {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class MassaDados {
+	private Workbook arquivo;
+	private Sheet planilha;
+	private FileInputStream fileInputStream;
+	private FileOutputStream fileOutputStream;
+
+	public MassaDados(Workbook arquivo, Sheet planilha) {
+		this.arquivo = arquivo;
+		this.planilha = planilha;
+	}
+
+	public MassaDados(String arquivoName, String planilhaName) {
+		try {
+			fileInputStream = new FileInputStream(new File(arquivoName));
+			arquivo = WorkbookFactory.create(fileInputStream);
+			planilha = arquivo.getSheet(planilhaName);
+		} catch (IOException e) {
+			throw new RuntimeException("Falha ao localizar ", e);
+		}
+	}
+
+	public String getCellData(int rowNumber, int columnNumber) {
+		Row row = planilha.getRow(rowNumber);
+		Cell cell = row.getCell(columnNumber);
+		DataFormatter formatter = new DataFormatter();
+		return formatter.formatCellValue(cell);
+	}
+
+	public int getRowCount() {
+		return planilha.getLastRowNum() + 1;
+	}
+
+	public int getColumnCount(int rowNumber) {
+		Row linhas = planilha.getRow(rowNumber);
+		return linhas.getLastCellNum();
+	}
+
+	public String[] getRowData(int rowNumber) {
+		Row row = planilha.getRow(rowNumber);
+		int columnCount = getColumnCount(rowNumber);
+		String[] rowData = new String[columnCount];
+
+		for (int coluna = 0; coluna < columnCount; coluna++) {
+			Cell cell = row.getCell(coluna);
+			DataFormatter formatter = new DataFormatter();
+			rowData[coluna] = formatter.formatCellValue(cell);
+		}
+
+		return rowData;
+	}
+
+	public void saveWorkbook(String arquivoName) {
+		try {
+			fileOutputStream = new FileOutputStream(new File(arquivoName));
+			arquivo.write(fileOutputStream);
+			fileOutputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+		try {
+			arquivo.close();
+			fileInputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
